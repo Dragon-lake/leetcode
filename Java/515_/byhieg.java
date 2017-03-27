2207888af175898624da6de8c44d7e3813ab8f53
byhieg <byhieg@gmail.com>
2017-03-27 14:17:35 +0800

    添加leetcode代码

---
+++ Java/515_/byhieg.java
@@ -0,0 +1,12 @@
+public class Solution {
+   int ans=0, h=0;
+    public int findBottomLeftValue(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return ans;
+    }
+    public void findBottomLeftValue(TreeNode root, int depth) {
+        if (h<depth) {ans=root.val;h=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1);
+    }
+}
